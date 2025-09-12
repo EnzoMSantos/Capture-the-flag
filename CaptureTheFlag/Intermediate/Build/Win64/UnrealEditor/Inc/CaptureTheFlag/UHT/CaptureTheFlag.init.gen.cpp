f553c9d006fb857d8c60c9399d357d532dc8b575
Enzo Moura <enzosantos2502@gmail.com>
2025-09-12 16:44:28 -0300

    sec commit

---
+++ CaptureTheFlag/Intermediate/Build/Win64/UnrealEditor/Inc/CaptureTheFlag/UHT/CaptureTheFlag.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeCaptureTheFlag_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_CaptureTheFlag;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_CaptureTheFlag()
+	{
+		if (!Z_Registration_Info_UPackage__Script_CaptureTheFlag.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/CaptureTheFlag",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xB9A47B56,
+				0x96B01DCC,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_CaptureTheFlag.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_CaptureTheFlag.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CaptureTheFlag(Z_Construct_UPackage__Script_CaptureTheFlag, TEXT("/Script/CaptureTheFlag"), Z_Registration_Info_UPackage__Script_CaptureTheFlag, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB9A47B56, 0x96B01DCC));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
